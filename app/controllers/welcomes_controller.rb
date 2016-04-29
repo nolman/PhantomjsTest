b0c5be333ce0aed92d8d6ad56962e4a5d236c3bd
Nolan <nolan@opendoor.com>
2016-04-29 14:12:34 -0700

    slow down seceond js

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,2 +1,8 @@
 class WelcomesController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def slow
+    sleep(1)
+    render :content_type => 'text/javascript'
+  end
 end
